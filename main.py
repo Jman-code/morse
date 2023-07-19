4b6173cb649d103af08219b1f5cafc0a3c126637
Jman-code <jman0413pdx@gmail.com>
2023-07-18 17:24:22 -0700

    messing around with ttk.bootstrap!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.ttk as ttk
-
+import ttkbootstrap as ttkb
+from ttkbootstrap.constants import *
 
 
 morse_code = {
@@ -27,37 +28,52 @@ def translator(text):
 
 def delete():
     entry.delete(0, tk.END)
-
+    morse.config(text='Waiting....')
 def update_greeting(text):
-    greeting.config(text='Morse Code: ' + text)
+    morse.config(text='Morse Code: ' + text)
 
 def submit():
     text = entry.get()
     morse = translator(text)
     update_greeting(morse)
 
+def update_progress(*args):  # args is needed as this is a callback from a trace
+    text_len = len(entry_str.get())
+    bar['value'] = (text_len / 20) * 100
+
+root = ttkb.Window(themename='cyborg')
+root.title('Morse Code Fun!')
+root.geometry('500x350')
 
 
-window = tk.Tk()
 
-frame = tk.Frame(bg='grey')
-frame.pack()
-greeting = tk.Label(master=frame, text='Enter your Text Below',
-                    width=50,
-                    height=25,
-                    fg='skyblue',
-                    bg='grey')
+
+greeting = ttkb.Label(root, text='Enter your Text Below',
+                    padding=10,
+                    bootstyle='info')
 greeting.pack()
 
-entry = tk.Entry(fg='black', bg='crimson', width=50)
+bar = ttkb.Progressbar(bootstyle='info')
+bar.pack(side=TOP)
+
+entry_str = StringVar()
+entry_str.trace("w", update_progress)
+
+
+entry = ttkb.Entry(bootstyle='light', width=50, textvariable=entry_str)
 entry.pack()
 
-frm_buttons = tk.Frame()
-frm_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
-btn_submit= tk.Button(master=frm_buttons, text='Submit', command=submit)
-btn_submit.pack(side=tk.RIGHT, padx=10, ipadx=10)
-btn_clear = tk.Button(master=frm_buttons, text='Clear', command=delete)
-btn_clear.pack(side=tk.RIGHT, ipadx=10)
+morse = ttkb.Label(root, text=' ', padding=20, font=42, bootstyle='danger')
+morse.pack()
 
-window.mainloop()
+frm_buttons = ttkb.Frame()
+frm_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
+
+btn_submit= ttkb.Button(master=frm_buttons, text='Submit', command=submit, bootstyle='success')
+btn_submit.pack(side=TOP, padx=10, ipadx=10)
+
+btn_clear = ttkb.Button(master=frm_buttons, text='Clear', command=delete, bootstyle='warning')
+btn_clear.pack(side=BOTTOM, ipadx=10)
+
+root.mainloop()
 
